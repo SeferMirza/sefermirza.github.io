679fc23fc9fb4b96a24c5e5c6ef7674b1f786d9b
Sefer Mirza <36925434+SeferMirza@users.noreply.github.com>
2023-02-03 16:49:23 +0300

    new

---
+++ _nuxt/Markdown.10876693.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.cb683c9e.js";import{j as o,k as u,D as f,M as c}from"./entry.cf4d1b82.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
